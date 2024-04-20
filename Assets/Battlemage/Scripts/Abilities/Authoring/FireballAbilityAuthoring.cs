3e0632cf80f6d45be154ad657be0a9437db1d5bf
Tobenai <akuapengin42@gmail.com>
2024-04-20 13:56:00 +1000

    More fireball testing

--- Assets/Battlemage/Scripts/Abilities/Authoring/FireballAbilityAuthoring.cs
+++ Assets/Battlemage/Scripts/Abilities/Authoring/FireballAbilityAuthoring.cs
@@ -1,9 +1,13 @@
-﻿using AOT;
+﻿using System.Runtime.InteropServices;
+using AOT;
 using Battlemage.GameplayBehaviours.Data;
 using Battlemage.GameplayBehaviours.Data.GameplayEvents;
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
 using UnityEngine.Scripting;
+using Waddle.Abilities.Data;
 using Waddle.GameplayBehaviour.Authoring;
 using Waddle.GameplayBehaviour.Data;
 
@@ -15,13 +19,26 @@ namespace Battlemage.Abilities.Authoring
         [GameplayEvent(typeof(GameplayOnSpawnEvent)), BurstCompile, Preserve, MonoPInvokeCallback(typeof(GameplayOnSpawnEvent.Delegate))]
         private static void OnSpawn(ref GameplayState state, ref Entity self)
         {
-            state.SetVelocity(self, state.GetForward(self) * 1.0f);
+            var abilityData = state.GetComponent<AbilityData>(self);
+            var playerTransform = state.GetComponent<LocalTransform>(abilityData.Source);
+            playerTransform.Position += playerTransform.Up() * 1.25f;
+            state.SetComponent(self, playerTransform);
+            state.SetVelocity(self, state.GetForward(self) * 20.0f);
+            
+            state.ScheduleEvent(self, 1.0f, nameof(DoExplode));
         }
+        
         
         [GameplayEvent(typeof(GameplayOnHitEvent)), BurstCompile, Preserve, MonoPInvokeCallback(typeof(GameplayOnHitEvent.Delegate))]
         private static void OnHit(ref GameplayState state, ref Entity self, ref Entity target)
         {
             //state.DealDamage(self, 1.0f, target);
+        }
+        
+        [GameplayEvent(typeof(GameplayScheduledEvent)), BurstCompile, Preserve, MonoPInvokeCallback(typeof(GameplayOnHitEvent.Delegate))]
+        private static void DoExplode(ref GameplayState state, ref Entity self)
+        {
+            state.MarkForDestroy(self);
         }
 
         public class FireballAbilityAuthoringBaker : Baker<FireballAbilityAuthoring>
--- Assets/Waddle/Abilities/Data/AbilityData.cs
+++ Assets/Waddle/Abilities/Data/AbilityData.cs
@@ -1,9 +1,12 @@
 ﻿using Unity.Entities;
+using Unity.NetCode;
 
 namespace Waddle.Abilities.Data
 {
+    [GhostComponent]
     public struct AbilityData : IComponentData
     {
+        [GhostField]
         public Entity Source;
     }
 }
